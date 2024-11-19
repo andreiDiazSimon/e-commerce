3653af8af734514d1fee25b4beb1714d4a14add1
andreiDiazSimon <andreisimon2004@gmail.com>
2024-11-19 21:03:05 +0800

    HomeUi

--- front-end/src/Home.jsx
+++ front-end/src/Home.jsx
@@ -1,10 +1,13 @@
 import './styles/Home.css'
-import HPbg from './assets/home-page-background-image.png';
+import HomeUi from './HomeUi'
 
+import { useState } from 'react'
 export default function Home() {
+
+	let [showIto, setShowIto] = useState('home')
+
 	return (
 		<div className="w-full">
-
 			{/* navbar Container */}
 			<div className="flex justify-evenly items-center bg-gradient-to-r from-[#171717] to-[#444444] w-full">
 				{/* Pogi (Logo) */}
@@ -12,9 +15,9 @@ export default function Home() {
 					<div className="w-[90px] h-[90px] bg-[url('./assets/logo.png')] bg-contain bg-no-repeat bg-center transition-all duration-300 hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.8)]"></div>
 				</div>
 
-				{/* Content Container (Menu items) */}
+				{/* navbar content */}
 				<div className="flex gap-[90px]">
-					<div className="nav-item">HOME</div>
+					<div className="nav-item" onClick={() => setShowIto('home')}>HOME</div>
 					<div className="nav-item">BROWSE</div>
 					<div className="nav-item">PROFILE</div>
 					<div className="nav-item">CHAT</div>
@@ -22,14 +25,7 @@ export default function Home() {
 				</div>
 			</div>
 
-			{/* Background Image (HPbg) */}
-			<div
-				className="w-full h-screen bg-cover bg-no-repeat bg-center"
-				style={{
-					backgroundImage: `url(${HPbg})`,
-					filter: "blur(15px)",
-				}}
-			></div>
+			{showIto == 'home' && <HomeUi />}
 		</div>
 	);
 }
---
+++ front-end/src/HomeUi.jsx
@@ -0,0 +1,19 @@
+import HPbg from './assets/home-page-background-image.png';
+const HomeUi = () => {
+	return (
+		<div
+			className=" text-white flex justify-start items-center w-full h-screen bg-cover bg-no-repeat bg-[center_left]"
+			style={{
+				backgroundImage: `url(${HPbg})`,
+			}}
+		>
+			<div className='ml-[3rem] p-[2em] mb-[3rem] bg-[#0000006b] w-[40%] flex flex-col justify-center items-center gap-[1rem] '>
+				<div className=' font-sans text-[3em] font-bold text-center'>Bridging skills to<br /> your service needs!</div>
+				<div className='font-mono tracking-[.25em]'>Try it!</div>
+				<div className='bg-[white] text-black p-[1rem] transition-all ease-in-out duration-[0.3s] hover:bg-[black] hover:text-[white] '>HIRE NOW</div>
+			</div>
+		</div>
+	);
+};
+
+export default HomeUi;
